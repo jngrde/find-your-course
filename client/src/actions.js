65f11c2b805f67b381fd6063ea289a8f396a376d
Jan Grade <jan.grade@gmail.com>
2018-10-03 15:57:55 +0200

    add async await

--- client/src/actions.js
+++ client/src/actions.js
@@ -8,16 +8,14 @@ const ACTIONS = {
 export const toggleBookmark = createAction(ACTIONS.TOGGLE_BOOKMARK)
 export const receiveCourses = createAction(ACTIONS.RECEIVE_COURSES)
 
-export const fetchCourses = () => dispatch => {
+export const fetchCourses = () => async dispatch => {
   const savedCourses = sessionStorage.getItem('courses')
   if (savedCourses) {
     dispatch(receiveCourses({ courses: JSON.parse(savedCourses) }))
   } else {
-    fetch('http://localhost:5000/api/courses/getall')
-      .then(res => res.json())
-      .then(courses => {
-        dispatch(receiveCourses({ courses }))
-      })
+    const res = await fetch('http://localhost:5000/api/courses/getall')
+    const courses = await res.json()
+    dispatch(receiveCourses({ courses }))
   }
 }
 
