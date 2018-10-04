8cbe24d17e6b1c6e26b5cc34e786452b583a61c7
Jan Grade <jan.grade@gmail.com>
2018-10-04 11:45:26 +0200

    bugfixing

--- client/src/actions.js
+++ client/src/actions.js
@@ -9,9 +9,14 @@ export const toggleBookmark = createAction(ACTIONS.TOGGLE_BOOKMARK)
 export const receiveCourses = createAction(ACTIONS.RECEIVE_COURSES)
 
 export const fetchCourses = () => async dispatch => {
-  const savedCourses = sessionStorage.getItem('courses')
-  if (savedCourses) {
-    dispatch(receiveCourses({ courses: JSON.parse(savedCourses) }))
+  let savedCourses
+  try {
+    savedCourses = JSON.parse(sessionStorage.getItem('courses'))
+  } catch (error) {
+    savedCourses = []
+  }
+  if (savedCourses.length) {
+    dispatch(receiveCourses({ courses: savedCourses }))
   } else {
     const res = await fetch('http://localhost:5000/api/courses/getall')
     const courses = await res.json()
