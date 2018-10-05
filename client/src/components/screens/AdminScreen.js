47459e6ebf80c649648f1c0acb2f1690d17d6121
Jan Grade <jan.grade@gmail.com>
2018-10-05 14:00:50 +0200

    starting admin-table

--- client/src/components/screens/AdminScreen.js
+++ client/src/components/screens/AdminScreen.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactTable from 'react-table'
 
 export default class AdminScreen extends Component {
+  static propTypes = {
+    courses: PropTypes.arrayOf(PropTypes.object),
+    loading: PropTypes.bool,
+    fetchCourses: PropTypes.any,
+  }
+  /*componentDidMount() {
+    this.props.fetchCourses()
+  }*/
+
+  renderTable() {
+    const { courses, loading } = this.props
+    const columns = [
+      {
+        Header: 'Titel',
+        accessor: 'title',
+      },
+      {
+        Header: 'Anbieter',
+        accessor: 'provider',
+      },
+      {
+        Header: 'Nummer',
+        accessor: 'number',
+      },
+      {
+        Header: 'Dauer',
+        accessor: 'duration',
+      },
+      {
+        Header: 'Kosten',
+        accessor: 'costs',
+      },
+      {
+        Header: 'Kurszeiten',
+        accessor: 'times',
+      },
+      {
+        Header: 'Lehrer',
+        accessor: 'teacher',
+      },
+      {
+        Header: 'Ort',
+        accessor: 'place',
+      },
+      {
+        Header: 'Beschreibung',
+        accessor: 'text',
+      },
+    ]
+
+    if (!loading) {
+      return (
+        <ReactTable
+          data={courses}
+          columns={columns}
+          manual
+          onFetchData={this.fetchCourses()}
+        />
+      )
+    }
+  }
+
   render() {
     return (
-      <div>
+      <React.Fragment>
         <h1>Hello, Admin</h1>
-      </div>
+        <React.Fragment>{this.renderTable()}</React.Fragment>
+      </React.Fragment>
     )
   }
 }
